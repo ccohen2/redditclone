f0fe95a1dc772fdfd7fbfb371a38d58a4416b688
Camron Cohen <ccohen2@oberlin.edu>
2022-03-07 11:34:46 -0500

    refactored code so user handling is separated into its own route

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const bcrypt = require("bcrypt");
 const mongoose = require("./required/mongoose");
 //routers
 const subredditRouter = require("./routes/subreddit");
+const sessionRouter = require("./routes/session");
 
 //express initialization
 const app = express();
@@ -49,76 +50,8 @@ app.set("views", path.join(__dirname, "/views"));
 //SUBREDDIT PAGES - all subroutes handled by router
 app.use("/r/:subreddit", subredditRouter);
 
-//users
-//registration
-app.get("/signup", (req, res, next) => {
-    res.render("signup.ejs");
-});
-
-app.post("/signup", asyncWrap(async (req, res, next) => {
-    const { username, password, confirmpassword } = req.body;
-    //checks passwords match
-    if (password != confirmpassword) {
-        throw new ClientError(500, "Passwords do not match!!", "user", "post");
-    }
-    const user = new User({username, password});
-
-    const finished = await user.save();
-    //checks user was saved to database
-    if (finished != user) {
-        throw new ClientError(500, `Unabled to create new user - server side error`, "user", "post");
-    }
-
-    //redirect to reddit homepage - once implemented homepage
-    res.send("Created new user!!");
-}));
-
-//user authentication
-//login
-app.get("/login", (req, res, next) => {
-    res.render("login.ejs", {"query": req.query.q});
-});
-
-app.post("/login", asyncWrap(async (req, res, next) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({username: username});
-    //checks user exists
-    if (user === null) {
-        throw new ClientError(404, `Unable fo find user ${username}`, "user", "get");
-    }
-
-    //logs user in
-    if (bcrypt.compare(password, user.password)) {
-        req.session.user = username;
-    }
-    else {
-        res.send("Invalid username or password!!!");
-    }
-
-    res.redirect(req.query.q);
-}));
-
-//logout
-app.get("/logout", (req, res, next) => {
-    try {
-        //checks if user is logged in - if so logs them out
-        if (req.session.user !== null && typeof (req.session.user) !== "undefined") {
-            req.session.user = null;
-        }
-        else if (req.session.user === null || typeof (req.session.user) === "undefined") {
-            throw new ClientError(403, "Not Logged in", "user", "post");
-        }
-    } 
-    //sets user to null - in case user variable in session hasn't been initialized
-    catch {
-        req.session.user = null;
-        res.send("NOT LOGGED IN");
-    }
-
-    //redirect to home page with flash message
-    res.redirect(req.query.q);
-});
-
+//users handling - login, logout, register
+app.use("/session", sessionRouter);
 
 
 //home - try moving this to /r but before /r/:subreddit - everything should work as long as no /r/:something in /r
---
+++ routes/session.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const mongoose = require("../required/mongoose");
+const { asyncWrap } = require("../required/helperFunctions")
+const { ClientError } = require("../required/errors");
+const { Post, Subreddit, User } = require("../required/schemas");
+const postRouter = require("./post");
+
+const router = express.Router({mergeParams: true});
+
+//registration
+router.get("/signup", (req, res, next) => {
+    res.render("signup.ejs");
+});
+
+router.post("/signup", asyncWrap(async (req, res, next) => {
+    const { username, password, confirmpassword } = req.body;
+    //checks passwords match
+    if (password != confirmpassword) {
+        throw new ClientError(500, "Passwords do not match!!", "user", "post");
+    }
+    const user = new User({username, password});
+
+    const finished = await user.save();
+    //checks user was saved to database
+    if (finished != user) {
+        throw new ClientError(500, `Unabled to create new user - server side error`, "user", "post");
+    }
+
+    //redirect to reddit homepage - once implemented homepage
+    res.send("Created new user!!");
+}));
+
+//user authentication
+//login
+router.get("/login", (req, res, next) => {
+    res.render("login.ejs", {"query": req.query.q});
+});
+
+router.post("/login", asyncWrap(async (req, res, next) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({username: username});
+    //checks user exists
+    if (user === null) {
+        throw new ClientError(404, `Unable fo find user ${username}`, "user", "get");
+    }
+
+    //logs user in
+    if (bcrypt.compare(password, user.password)) {
+        req.session.user = username;
+    }
+    else {
+        res.send("Invalid username or password!!!");
+    }
+
+    res.redirect(req.query.q);
+}));
+
+//logout
+router.get("/logout", (req, res, next) => {
+    try {
+        //checks if user is logged in - if so logs them out
+        if (req.session.user !== null && typeof (req.session.user) !== "undefined") {
+            req.session.user = null;
+        }
+        else if (req.session.user === null || typeof (req.session.user) === "undefined") {
+            throw new ClientError(403, "Not Logged in", "user", "post");
+        }
+    } 
+    //sets user to null - in case user variable in session hasn't been initialized
+    catch {
+        req.session.user = null;
+        res.send("NOT LOGGED IN");
+    }
+
+    //redirect to home page with flash message
+    res.redirect(req.query.q);
+});
+
+module.exports = router;
